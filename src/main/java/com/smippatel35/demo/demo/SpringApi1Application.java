e45a1b583c839c3726bb3cf34b2b3157e892c308
Smit Patel <smitppatel35@gmail.com>
2021-01-08 16:53:31 +0530

    Intial Commit

---
+++ src/main/java/com/smippatel35/demo/demo/SpringApi1Application.java
@@ -0,0 +1,13 @@
+package com.smippatel35.demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/smippatel35/demo/demo/SpringApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smippatel35.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
