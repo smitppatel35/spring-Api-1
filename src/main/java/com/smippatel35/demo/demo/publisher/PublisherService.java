bca19bafca559b1278fe252c0f125bd0475ce492
Smit Patel <smitppatel35@gmail.com>
2021-01-09 22:17:08 +0530

    Publisher get API Working!

--- src/main/java/com/smippatel35/demo/demo/publisher/PublisherService.java
+++ src/main/java/com/smippatel35/demo/demo/publisher/PublisherService.java
@@ -1,8 +1,11 @@
 package com.smippatel35.demo.demo.publisher;
 
 import com.smippatel35.demo.demo.exception.LibraryResourceAlreadyExistException;
+import com.smippatel35.demo.demo.exception.LibraryResourceNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PublisherService {
@@ -13,7 +16,7 @@ public class PublisherService {
         this.publisherRepository = publisherRepository;
     }
 
-    public Publisher addPublisher(Publisher publisherToBeAdded) throws LibraryResourceAlreadyExistException {
+    public void addPublisher(Publisher publisherToBeAdded) throws LibraryResourceAlreadyExistException {
 
         PublisherEntity publisherEntity = new PublisherEntity(
                 publisherToBeAdded.getName(),
@@ -29,8 +32,26 @@ public class PublisherService {
         }
 
         publisherToBeAdded.setPublisherId(addedPublisher.getPublisherId());
-
-        return publisherToBeAdded;
     }
 
+    public Publisher getPublisher(Integer publisherId) throws LibraryResourceNotFoundException {
+
+        Optional<PublisherEntity> publisherEntity = publisherRepository.findById(publisherId);
+
+        Publisher publisher = null;
+
+        if (publisherEntity.isPresent()){
+
+            PublisherEntity pe = publisherEntity.get();
+            publisher = createPublisherFromEntity(pe);
+        } else {
+            throw new LibraryResourceNotFoundException("Publisher Id: "+ publisherId +" Not Found!!");
+        }
+
+        return publisher;
+    }
+
+    private Publisher createPublisherFromEntity(PublisherEntity pe) {
+        return new Publisher(pe.getPublisherId(), pe.getName(), pe.getEmailId(), pe.getPhoneNumber());
+    }
 }
